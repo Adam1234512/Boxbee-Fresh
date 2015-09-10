e7c979f36f3ba489f26db7dba3f12b36e2402595
kristophM <krist.m@gmail.com>
2015-09-10 13:53:43 -0400

    Wrote some comments in migrations

--- db/migrate/20150906025447_add_guest_to_company.rb
+++
@@ -1,5 +0,0 @@
-class AddGuestToCompany < ActiveRecord::Migration
-  def change
-    add_reference :companies, :guest, index: true, foreign_key: true
-  end
-end
--- db/migrate/20150907054107_drop.rb
+++ db/migrate/20150907054107_drop.rb
@@ -1,5 +1,6 @@
 class Drop < ActiveRecord::Migration
   def change
+    #Order of names in many to many table was incorrect so added this drop and add w/ correct name. 
     drop_table :companies_cities do |t|
       t.belongs_to :company, index: true
       t.belongs_to :city, index: true
