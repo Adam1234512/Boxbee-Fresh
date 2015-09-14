55356b942890f53d18133ba8866b042cf4510497
kristophM <krist.m@gmail.com>
2015-09-14 18:08:55 -0400

    remove unneeded dependencies in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,8 +15,5 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require beta_surveys
-//= require twitter/typeahead
-//= require bootstrap-tagsinput
 //= require jquery-ui
-//= require autocomplete-rails
 //= require_tree .
